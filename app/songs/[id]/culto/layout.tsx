28592cca1be94e94b68764473a2b6248ec790b49
jadsonogueira <108072743+jadsonogueira@users.noreply.github.com>
2026-01-25 18:00:48 -0500

    Criar layout.tsx

---
+++ app/songs/[id]/culto/layout.tsx
@@ -0,0 +1,11 @@
+export const dynamic = "force-dynamic";
+
+export default function CultoLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body className="min-h-screen bg-white text-black dark:bg-black dark:text-white">
+        {children}
+      </body>
+    </html>
+  );
+}
