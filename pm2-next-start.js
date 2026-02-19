6a6bf6dad019392c45d8b5a23c9291251835f68f
jadsonogueira <108072743+jadsonogueira@users.noreply.github.com>
2026-02-19 06:24:15 +0100

    Criar pm2-next-start.js

---
+++ pm2-next-start.js
@@ -0,0 +1,13 @@
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
+
+const { spawn } = require("child_process");
+
+const port = process.env.PORT || "3001";
+
+const child = spawn(
+  process.execPath,
+  [".\\node_modules\\next\\dist\\bin\\next", "start", "-p", port],
+  { stdio: "inherit", shell: true, cwd: process.cwd() }
+);
+
+child.on("exit", (code) => process.exit(code ?? 0));
